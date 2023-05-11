1db93a917dda85efe3f99d9321e4914856abd645
FrozstDev <frozstdev@gmail.com>
2023-05-11 19:18:25 -0400

    Logic finished

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 ### TIC TAC TOE
+
+Aprender spread y rest operator
+
+NOTA:
+Importante - La actualizacion de los estados en React son ASINCRONOS - Todos los hooks(STATES) NO deben estar dentro de un IF, deben estar en el cuerpo del componente. NO DEBEN ESTAR EN UN IF, WHILE ...ETC - Mejor forma de recuperacion del local storage es inyectar una funcion inicializadora para el useState useState(() => {})
+Leer el localstorage desde el componente es LENTO, ademas renderiza una app por muchas veces MALA PRACTICA - || => esto mira si el elemnto es false
+?? => esto mira si el elemnto es null or undefined - Buena practica : Es mejor el item identificado del LOCALSTORAGE, que todos con el comando clean() - Segundo hook mas importate de React es UseEffect => Es un hook que nos permite ejecutar codigo arbitrario (cualquier codigo) cuando el COMPONENTE se monta en el DOM y cada vez que se cambia las dependencias que nosotros le digamos
+Se ejecuta dentro del cuerpo del componente
+
+NOTA: No hacer pruebas tecnicas sin linter (IMPORTANTE)
+Usaremos Standard js
+npm install standard -D
+Configuramos el pakage.json
+"eslintConfig": {
+"extends": "./node_modules/standard/eslintrc.json"
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,82 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState, useEffect } from 'react'
+import confetti from 'canvas-confetti'
+import { Square } from './components/Square.jsx'
+import { TURNS } from './constans.js'
+import { checkWinnerFrom, checkEndGame } from './logic/board.js'
+import { WinnerModal } from './components/WinnerModal.jsx'
+import { saveGameToStorage, resetGameStorage } from './logic/storage/index.js'
 
-function App() {
+function App () {
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  })
 
-  return <h1>Tic Tac Toe</h1>;
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
+
+  const [winner, setWinner] = useState(null) // Null no hay ganador, false empate
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return // Verificar si hay algun dato guardado
+
+    const newBoard = [...board] // spread operator
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    // TODO Guardar localmente la partida
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn
+    })
+    // Revisar Ganador
+    const newWinner = checkWinnerFrom(newBoard)
+    if (newWinner) {
+      confetti()
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
+    }
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+    resetGameStorage()
+  }
+
+  useEffect(() => {
+    console.log('UseEffect')
+  }, [])
+
+  return (
+    <main className='board'>
+      <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Reiniciar el juego</button>
+      <section className='game'>
+        {board.map((cell, index) => {
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {cell}
+            </Square>
+          )
+        })}
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+
+      <WinnerModal winner={winner} resetGame={resetGame} />
+    </main>
+  )
 }
 
-export default App;
+export default App
